a48bff580f0b89a82f4e77775a6c4c9c01781b34
Tatiana Savina <tatianasavina@MacBook-Pro-Savka.local>
2018-05-02 19:23:45 +0300

    second

---
+++ Iwantpython.py
@@ -0,0 +1,20 @@
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater(token='524090412:AAHC6mjNhViDYgjgd1fmeg1TGo_B_QYL0gE')
+dispatcher = updater.dispatcher
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Hi')
+def textMessage(bot, update):
+    response = 'Got your message' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+
+updater.start_polling(clean=True)
+
+updater.idle()
